85e6c244802dd1db941bd3e2bb8c064ec801f1ba
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-13 15:38:37 +0200

    age

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+
+// Richiesta informazioni
 const nameUser = prompt('Inserisci il tuo nome');
 const surnameUser = prompt('Inserisci il tuo cognome');
 const colorUser = prompt('Inserisci il tuo colore');
 
-console.log(nameUser, surnameUser ,colorUser);
+// Bonus
+const ageUser = prompt('inserisci la tua età')
 
+// Scrittura su console
+console.log(nameUser, surnameUser ,colorUser, ageUser);
 
+// Scrittura su pagina web 
+
+// Nome
 document.getElementById("name-user").innerHTML = nameUser
+// Cognome
 document.getElementById("surname-user").innerHTML = surnameUser
+// Colore
 document.getElementById("color-user").innerHTML = colorUser
+// Età
+document.getElementById("age-user").innerHTML = ageUser
 
